c7be5b45431484abed187bf372fae7350432e958
Ana Carolina Pio <110706362+Ana-Carolina-Pio@users.noreply.github.com>
2024-03-20 22:12:37 -0300

    feature/confBasica

--- chess/admin.py
+++ chess/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+
+from chess.models import Time
 # Register your models here.
+
+
+class TimeAdmin(admin.ModelAdmin):
+    list_display = ("nome", "color1", "color2", "branding")
+    search_fields = ("nome",)
+
+
+admin.site.register(Time, TimeAdmin)
